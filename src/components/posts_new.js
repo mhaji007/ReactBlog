d124b76704b3ad9db3dfc06575c2a8ad69436dba
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-23 20:41:08 -0400

    Add Field component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,9 +4,32 @@ import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
     state = {  }
+
+    renderTitleField(field) {
+        <div>
+            <input
+            type="text"
+            /// onChange ={field.input.onChange}
+            /// onFocus ={field.input.onFocus}
+            /// onBlur ={field.input.onBlur}
+            /// Following is shorthand notation
+            /// for the above syntax
+                {...field.input}
+            />
+        </div>
+    }
+
     render() { 
         return ( 
             <form >
+                <Field name="title"
+                        component = {this.renderTitleField}
+
+                />
+                <Field name="title"
+                        component = {}
+
+                />
                 <Field name="title"
                         component = {}
 
