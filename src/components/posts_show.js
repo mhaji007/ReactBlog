d5059ce73a3a58edd3597ea2c4b31a052b9931d2
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-24 16:17:40 -0400

    Add PostsShow component

---
+++ src/components/posts_show.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+ 
+class PostsShow extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>
+                Posts Show!
+            </div>
+          );
+    }
+}
+ 
+export default PostsShow;
