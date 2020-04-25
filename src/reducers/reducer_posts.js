8f5f5aaa465a8ee608d1fd44c65860e492b524fb
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-24 21:58:21 -0400

    Add and wire up fetchPost action creator

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,9 +4,17 @@ import {FETCH_POSTS} from '../actions';
 
 export default function (state={}, action) {
     switch(action.type) {
-        case FETCH_POSTS:
+    case FETCH_POST:
+            // const post = action.payload.data;
+            // const newState = {...state };
+            // newState[post.id] = post;
+            // return newState;
+
+        return {...state, [action.payload.data.id]: action.payload.data};
+
+    case FETCH_POSTS:
         return _.mapKeys(action.payload.data, 'id');
-        default:
-            return state;
+    default:
+        return state;
     }
 }
